bc95e06dd154b75907f8c622f959929a9032a421
kaua <kauarezende416@gmail.com>
2026-03-25 13:41:07 -0300

    Primeiro Endpoint

--- Program.cs
+++ Program.cs
@@ -1,6 +1,27 @@
+using Microsoft.Extensions.Localization;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.MapGet("/", () => "Rodando API!");
+
+app.MapGet("/usuarios", () => "Lista de usuários");
+app.MapPost("/usuarios", (string nome) =>
+{
+    return $"Usuario {nome} criado";
+});
+
+
 
 app.Run();
